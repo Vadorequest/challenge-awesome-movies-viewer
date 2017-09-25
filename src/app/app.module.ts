bdbb08dfff7f8f3a78c20e7d8b39f0bca50b0d34
Vadorequest <ambroise.dhenain@gmail.com>
2017-09-25 17:13:23 +0200

    Add Angular Material UI framework with basic working design (top toolbar) and theming.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MdToolbarModule, MdFormFieldModule, MdInputModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { MoviesListComponent } from './movies-list/movies-list.component';
 import { MovieListItemComponent } from './movie-list-item/movie-list-item.component';
 import { MovieCardComponent } from './movie-card/movie-card.component';
+
+import 'hammerjs';
 
 @NgModule({
   declarations: [
@@ -16,7 +20,11 @@ import { MovieCardComponent } from './movie-card/movie-card.component';
     MovieCardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MdToolbarModule,
+    MdFormFieldModule,
+    MdInputModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
