d92c98a4067ab321c486be1b3318ca734317854a
Vadorequest <ambroise.dhenain@gmail.com>
2017-09-25 15:00:41 +0200

    Add skeleton components

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# AngularIoTest1
+# Awesome Movies Viewer (alpha)
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 1.4.3.
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MoviesListComponent } from './movies-list/movies-list.component';
+import { MovieListItemComponent } from './movie-list-item/movie-list-item.component';
+import { MovieCardComponent } from './movie-card/movie-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MoviesListComponent,
+    MovieListItemComponent,
+    MovieCardComponent
   ],
   imports: [
     BrowserModule
